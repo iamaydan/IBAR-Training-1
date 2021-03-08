2d147767aeef01d1503402d9c8dd8c394cdcc816
Aydan Gozalova <aydangozalova@gmail.com>
2021-03-08 19:37:15 +0400

    ViewModel Factory added
    Signed-off-by: Aydan Gozalova <aydangozalova@gmail.com>

--- Training-1/ViewModelDemo2/app/src/main/java/com/example/viewmodeldemo2/MainActivity.kt
+++ Training-1/ViewModelDemo2/app/src/main/java/com/example/viewmodeldemo2/MainActivity.kt
@@ -9,11 +9,13 @@ import com.example.viewmodeldemo2.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainActivityViewModel
+    private lateinit var viewModelFactory: MainActivityViewModelFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        viewModelFactory = MainActivityViewModelFactory(9)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
 
         binding.resultText.text = viewModel.getCount().toString()
 
--- Training-1/ViewModelDemo2/app/src/main/java/com/example/viewmodeldemo2/MainActivityViewModel.kt
+++ Training-1/ViewModelDemo2/app/src/main/java/com/example/viewmodeldemo2/MainActivityViewModel.kt
@@ -2,8 +2,12 @@ package com.example.viewmodeldemo2
 
 import androidx.lifecycle.ViewModel
 
-class MainActivityViewModel : ViewModel() {
+class MainActivityViewModel(startingCount: Int) : ViewModel() {
     private var count = 0
+
+    init {
+        count = startingCount
+    }
 
     fun getCount(): Int {
         return count
