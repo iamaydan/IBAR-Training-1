579792eb3951aab688544ff063884ba14263ce85
Aydan Gozalova <aydangozalova@gmail.com>
2021-03-08 18:25:28 +0400

    initial commit
    Signed-off-by: Aydan Gozalova <aydangozalova@gmail.com>

---
+++ Training-1/ViewModelDemo1/app/build/generated/ap_generated_sources/debug/out/com/anushka/viewmodeldemo1/BR.java
@@ -0,0 +1,5 @@
+package com.anushka.viewmodeldemo1;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ Training-1/ViewModelDemo1/app/build/generated/data_binding_trigger/debug/com/anushka/viewmodeldemo1/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.anushka.viewmodeldemo1;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
