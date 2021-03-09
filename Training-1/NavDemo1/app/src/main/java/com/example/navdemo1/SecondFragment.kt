5534125b4f0d6ffa477ab8008bf741fda8e2cd20
Aydan Gozalova <aydangozalova@gmail.com>
2021-03-09 23:22:01 +0400

    Added : Second fragment, navigation with bundle
    Signed-off-by: Aydan Gozalova <aydangozalova@gmail.com>

---
+++ Training-1/NavDemo1/app/src/main/java/com/example/navdemo1/SecondFragment.kt
@@ -0,0 +1,25 @@
+package com.example.navdemo1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.navdemo1.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+        val input: String? = arguments?.getString("user_input")
+        binding.inputText.text = input
+
+        return binding.root
+    }
+
+}
