a24105f3f387f0b318127faf53e39126ab2a65a3
Aydan Gozalova <aydangozalova@gmail.com>
2021-03-12 01:39:18 +0400

    Fragments added
    Signed-off-by: Aydan Gozalova <aydangozalova@gmail.com>

---
+++ NavDemo1/app/src/main/java/com/anushka/navdemo1/HomeFragment.kt
@@ -0,0 +1,36 @@
+package com.anushka.navdemo1
+
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.anushka.navdemo1.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        binding.submitButton.setOnClickListener {
+
+            if (!TextUtils.isEmpty(binding.inputText.text.toString())) {
+                val bundle = bundleOf("input" to binding.inputText.text.toString())
+                it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment, bundle)
+            } else {
+                Toast.makeText(activity, "Please insert your name", Toast.LENGTH_LONG).show()
+            }
+        }
+        return binding.root
+    }
+}
--- NavDemo1/app/src/main/java/com/anushka/navdemo1/MainActivity.kt
+++ NavDemo1/app/src/main/java/com/anushka/navdemo1/MainActivity.kt
@@ -2,13 +2,15 @@ package com.anushka.navdemo1
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import com.anushka.navdemo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
