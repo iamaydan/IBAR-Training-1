5a08b8bf2f36602bfa3e4b68ed1f7b9a4f72f01d
Aydan Gozalova <aydangozalova@gmail.com>
2021-03-12 21:24:48 +0400

    Suspenddemo added
    Signed-off-by: Aydan Gozalova <aydangozalova@gmail.com>

---
+++ CoroutinesDemo1/app/src/main/java/com/anushka/coroutinesdemo1/SuspendDemo.kt
@@ -0,0 +1,12 @@
+package com.anushka.coroutinesdemo1
+
+class SuspendDemo {
+
+    private fun firstFunction() {
+        //code
+    }
+
+    private suspend fun secondFunction() {
+        //code
+    }
+}
