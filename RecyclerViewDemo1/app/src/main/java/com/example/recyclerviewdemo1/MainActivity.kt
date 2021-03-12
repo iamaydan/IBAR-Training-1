a2cc900c50f2f381b3f46af85ba7ba22a79879e6
Aydan Gozalova <aydangozalova@gmail.com>
2021-03-12 19:31:55 +0400

    Added list item class and xml, adapter
    Signed-off-by: Aydan Gozalova <aydangozalova@gmail.com>

--- RecyclerViewDemo1/app/src/main/java/com/example/recyclerviewdemo1/MainActivity.kt
+++ RecyclerViewDemo1/app/src/main/java/com/example/recyclerviewdemo1/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.recyclerviewdemo1
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val fruitsList = listOf(
+        Fruit("Mango", "Tom"),
+        Fruit("Apple", "Joe"),
+        Fruit("Banana", "Mark"),
+        Fruit("Guava", "Mike"),
+        Fruit("Lemon", "Mike"),
+        Fruit("Pear", "Frank"),
+        Fruit("Orange", "Joe")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        my_recycler_view.setBackgroundColor(Color.DKGRAY)
+        my_recycler_view.layoutManager = LinearLayoutManager(this)
+        my_recycler_view.adapter = MyRecyclerViewAdapter(fruitsList)
     }
+
 }
