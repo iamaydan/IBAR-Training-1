3df570e48788ac20cae8a388c4534538835e3ffb
Aydan Gozalova <aydangozalova@gmail.com>
2021-03-13 11:30:29 +0400

    Unstructured Concurrency
    Signed-off-by: Aydan Gozalova <aydangozalova@gmail.com>

--- CoroutinesDemo1/app/src/main/java/com/anushka/coroutinesdemo1/MainActivity.kt
+++ CoroutinesDemo1/app/src/main/java/com/anushka/coroutinesdemo1/MainActivity.kt
@@ -1,37 +1,35 @@
 package com.anushka.coroutinesdemo1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnCount.setOnClickListener {
             tvCount.text = count++.toString()
+
         }
         btnDownloadUserData.setOnClickListener {
 
-            CoroutineScope(Dispatchers.IO).launch {
-                downloadUserData()
+            CoroutineScope(Dispatchers.Main).launch {
+                tvUserMessage.text = UserDataManager().getTotalUserCount().toString()
             }
 
         }
     }
 
-    private suspend fun downloadUserData() {
+    private fun downloadUserData() {
         for (i in 1..200000) {
-            withContext(Dispatchers.Main) {
-                tvUserMessage.text = "Downloading user $i in ${Thread.currentThread().name}"
-            }
+            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
         }
     }
 }
