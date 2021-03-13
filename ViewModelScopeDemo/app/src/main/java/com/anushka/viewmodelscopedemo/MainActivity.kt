c403402fa7d4f2be7f1185b8971c4987250fc496
Aydan Gozalova <aydangozalova@gmail.com>
2021-03-13 19:33:07 +0400

    Models added

--- ViewModelScopeDemo/app/src/main/java/com/anushka/viewmodelscopedemo/MainActivity.kt
+++ ViewModelScopeDemo/app/src/main/java/com/anushka/viewmodelscopedemo/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.anushka.viewmodelscopedemo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var mainActivityViewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mainActivityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        mainActivityViewModel.getUserData()
+
+        mainActivityViewModel.users.observe(this, Observer { myUsers ->
+            myUsers.forEach {
+                Log.i("MyTag", " name is ${it.name}")
+            }
+
+        })
     }
 }
--- ViewModelScopeDemo/app/src/main/java/com/anushka/viewmodelscopedemo/MainActivityViewModel.kt
+++ ViewModelScopeDemo/app/src/main/java/com/anushka/viewmodelscopedemo/MainActivityViewModel.kt
@@ -1,30 +1,27 @@
 package com.anushka.viewmodelscopedemo
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.CoroutineScope
+import androidx.lifecycle.viewModelScope
+import com.anushka.viewmodelscopedemo.model.User
+import com.anushka.viewmodelscopedemo.model.UserRepository
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class MainActivityViewModel : ViewModel() {
-
-    private val myJob = Job()
-    private val myScope = CoroutineScope(Dispatchers.IO + myJob)
+    private var userRepository = UserRepository()
+    var users: MutableLiveData<List<User>> = MutableLiveData()
 
     fun getUserData() {
-
-        myScope.launch {
-            //write some code
+        viewModelScope.launch {
+            var result: List<User>? = null
+            withContext(Dispatchers.IO) {
+                result = userRepository.getUsers()
+            }
+            users.value = result
         }
-
     }
-
-    override fun onCleared() {
-        super.onCleared()
-        myJob.cancel()
-
-    }
-
 
 }
