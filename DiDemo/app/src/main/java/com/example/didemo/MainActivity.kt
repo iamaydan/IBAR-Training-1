5fe9dac87b667d888fb78303205a86fe7c854d9e
Aydan Gozalova <aydangozalova@gmail.com>
2021-03-16 09:58:12 +0400

    pure dependency injection

--- DiDemo/app/src/main/java/com/example/didemo/MainActivity.kt
+++ DiDemo/app/src/main/java/com/example/didemo/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.didemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        SmartPhone(
+                Battery(),
+                SIMCard(ServiceProvider()),
+                MemoryCard()
+        )
+                .makeACallWithRecording()
+
+
     }
 }
