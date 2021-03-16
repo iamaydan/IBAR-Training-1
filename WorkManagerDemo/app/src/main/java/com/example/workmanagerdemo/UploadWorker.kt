e206c0fc2f3518c81efc3bc1c7b8c75669da6b7d
Aydan Gozalova <aydangozalova@gmail.com>
2021-03-16 09:32:36 +0400

    set i/o data

--- WorkManagerDemo/app/src/main/java/com/example/workmanagerdemo/UploadWorker.kt
+++ WorkManagerDemo/app/src/main/java/com/example/workmanagerdemo/UploadWorker.kt
@@ -2,18 +2,35 @@ package com.example.workmanagerdemo
 
 import android.content.Context
 import android.util.Log
+import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import java.text.SimpleDateFormat
+import java.util.*
 
 class UploadWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
+
+    companion object {
+        const val KEY_WORKER = "key_worker"
+    }
+
     override fun doWork(): Result {
-        return try {
-            for (i in 0..600) {
-                Log.i("MyTag", "Uploading $i")
+        try {
+            val count = inputData.getInt(MainActivity.KEY_COUNT_VALUE, 0)
+            for (i in 0 until count) {
+                Log.i("mytag", "Uploading $i")
             }
-            Result.success()
+
+            val time = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+            val currentDate = time.format(Date())
+
+            val outPutData = Data.Builder()
+                .putString(KEY_WORKER, currentDate)
+                .build()
+
+            return Result.success(outPutData)
         } catch (e: Exception) {
-            Result.failure()
+            return Result.failure()
         }
     }
 }
