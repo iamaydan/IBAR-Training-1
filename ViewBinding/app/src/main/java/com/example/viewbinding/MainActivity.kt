2a2dbeabb4a819c931f60bcc5152bddc4931434f
Aydan Gozalova <aydangozalova@gmail.com>
2021-03-16 15:18:01 +0400

    added viewBinding

--- ViewBinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
+++ ViewBinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.viewbinding
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.submitButton.setOnClickListener {
+            displayGreeting()
+        }
+    }
+
+    private fun displayGreeting() {
+        binding.apply {
+            greetingTextView.text = "Hello! " + nameEditText.text
+        }
+
     }
 }
